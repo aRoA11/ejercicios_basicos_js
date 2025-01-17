0ced1cbc50e035df48839a878f9afeb97c66aa37
aRoA11 <trigoramos.aroa@gmail.com>
2025-01-17 15:06:34 +0100

    ejercicio 7

---
+++ ejercicio7.js
@@ -0,0 +1,10 @@
+let numberOne = 2
+let numberTwo = 6
+function greaterNumber(numberOne = 2, numberTwo = 6) {
+  if (numberOne > numberTwo) {
+    console.log(numberOne)
+  } 
+  else (numberTwo > numberOne) {
+    console.log(numberTwo)
+  }
+}
