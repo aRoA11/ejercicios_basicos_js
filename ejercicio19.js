6a9465d445d00f9c7e2ecf7236971edeb441786d
aRoA11 <trigoramos.aroa@gmail.com>
2025-01-23 12:43:57 +0100

    ejercicio 19

--- ejercicio19.js
+++ ejercicio19.js
@@ -7,3 +7,13 @@ const toys = [
   { id: 60, name: 'Nerf Blaster' },
   { id: 71, name: 'Sylvanian Families - Familia gato' }
 ]
+function hola(list) {
+  const toys.name = toy,
+  for (const name of toys) {
+    let item = list[i]
+    if (!toy.inludes('gato')) {
+      toy.push (item)
+    }
+  }
+}
+console.log(hola(toys))
